239646bc5400ff160f78593829a2ef342985d22e
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-10-18 20:56:24 -0300

    first commit

---
+++ myap/src/test/java/com/hugo/myapi/MyapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
